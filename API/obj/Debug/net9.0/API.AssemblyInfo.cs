7472b79516fc60f38a46bf2581a996fb95a0f76d
onuryilmaaz <onuryilm.41@gmail.com>
2025-05-09 16:52:06 +0300

    chore: update assembly information and related binaries for versioning

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f597ca2698efc6ebec92e08f0ca9ae11b2607e93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa348caeb656677b663c7b168b24f111d26b8207")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
